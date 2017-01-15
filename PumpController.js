fcc9ddb24d18dfc3682126d562bcc11fc4a767be
Konstantin-home <kd@freetrailer.dk>
2017-01-15 23:56:38 +0100

    socket io

--- PumpController.js
+++ PumpController.js
@@ -1,23 +1,25 @@
 var five = require('johnny-five');
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
 var moment = require('moment');
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
 
 
-app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '/index.html')
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-const port = process.env.PORT || 3000;
-
-
-server.listen(port);
-console.log(`Server listening on http://localhost:${port}`);
-
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 var board = new five.Board({
   port: "COM4",
@@ -32,7 +34,7 @@ var state = {
    "counter": 0,
    // when to water
    "wateringTimes": [moment().add(10, "s").format('HH:mm:ss'), moment().add(30, "s").format('HH:mm:ss'), moment().add(50, "s").format('HH:mm:ss'), moment().add(70, "s").format('HH:mm:ss')]
-   //"wateringTimes": ["08:00:00", "12:00:00", "16:00:00", "18:00:00", "22:43:00"]
+   //"wateringTimes": ["13:37:00", "12:00:00", "16:00:00", "18:00:00", "22:43:00"]
  },
    "system": {
      // tick for loop
@@ -48,6 +50,8 @@ board.on("ready", () => {
   board.loop(state.system.tick, function() {
     loop(relay);
   });
+
+
 });
 
 // on app close
@@ -56,35 +60,38 @@ board.on("exit", () => {
   var relay = new five.Relay(13);
   relay.close();
 });
-
-board.on("init", function() {
-  console.log("Connected");
-});
+// TODO: research when is fixed
+// board.on("connect", function() {
+//   console.log("Connected");
+// });
 
 function loop(relay) {
-  if (shouldPumpRun()) {
-    runPump(relay);
+  if (shouldRun()) {
+    run(relay);
   }
-  if (state.pump.running) {
+  if (shouldStop()) {
     eventEmitter.emit('pumping');
-    shouldPumpStop(relay);
+    stop(relay);
   }
 };
 
-function runPump(relay) {
+function run(relay) {
   eventEmitter.emit('pumpOn');
   state.pump.running = true;
   relay.open();
 };
 
-function stopPump(relay) {
+function stop(relay) {
   eventEmitter.emit('pumpOff');
   state.pump.running = false;
   state.pump.counter = 0;
   relay.close();
 };
 
-function shouldPumpRun() {
+/* shouldRun
+    return boolean
+*/
+function shouldRun() {
   var now = getTime();
   var wateringTimesLocal = state.pump.wateringTimes;
   for(var i = 0; i < wateringTimesLocal.length; i++) {
@@ -95,12 +102,16 @@ function shouldPumpRun() {
   return false;
 };
 
-function shouldPumpStop(relay) {
+function shouldStop() {
+  if (!state.pump.running) {
+    return false
+  }
   state.pump.counter++;
   pumpCounterToMilliseconds = state.pump.counter * 1000;
   if (pumpCounterToMilliseconds === state.pump.duration) {
-    stopPump(relay);
+    return true
   }
+  return false
 };
 
 function getTime() {
