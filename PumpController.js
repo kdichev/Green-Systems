b426674d253a7105e7ada803a0cafb4fedbe0ec3
Konstantin-home <kd@freetrailer.dk>
2017-01-16 00:21:57 +0100

    instead of run just toggle the relay

--- PumpController.js
+++ PumpController.js
@@ -48,7 +48,7 @@ board.on("ready", () => {
   io.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
     socket.on('my other event', function (data) {
-    run(relay);
+    relay.toggle();
       console.log(data);
     });
   });
